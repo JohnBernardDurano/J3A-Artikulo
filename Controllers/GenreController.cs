25a0e993f474142da23d8a4e760a64f40fe000a1
John Bernard Durano <jsdurano@student.apc.edu.ph>
2023-10-01 21:52:54 +0800

    Routing Done

---
+++ Controllers/GenreController.cs
@@ -0,0 +1,17 @@
+﻿using ENTJAVA_Week3.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using ENTJAVA_Week3.Models.EntityManager;
+
+namespace ENTJAVA_Week3.Controllers
+{
+    public class GenreController : Controller
+    {
+        public ActionResult AddGenre()
+        {
+            return View();
+        }
+
+
+
+    }
+}
